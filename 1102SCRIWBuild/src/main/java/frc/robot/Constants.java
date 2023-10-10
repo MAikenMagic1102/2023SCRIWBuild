778e832e8aa83e38b7c185d67686cfe7f250a84f
RwF124 <146141623+RwF124@users.noreply.github.com>
2023-10-10 19:40:53 -0400

    Updated constants file
    Added variables for motors, as well as setpoints.

--- 1102SCRIWBuild/src/main/java/frc/robot/Constants.java
+++ 1102SCRIWBuild/src/main/java/frc/robot/Constants.java
@@ -15,5 +15,50 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+
+    public static int steerFrontLeft = 10;
+    public static int steerFrontRight = 11;
+    public static int steerBackLeft = 12;
+    public static int steerBackRight = 13;
+
+    public static int driveFrontLeft = 14;
+    public static int driveFrontRight = 15;
+    public static int driveBackLeft = 16;
+    public static int driveBackRight = 17;
+
+    public static int frontLeftCANcoder = 18;
+    public static int frontRightCANcoder = 19;
+    public static int backLeftCANcoder = 20;
+    public static int backRightCANcoder = 21;
+    public static int driveMotorCurrentLimit = 55;
+    public static int steerMotorCurrentLimit = 45;
+    public static int driveController = 0;
+    public static int operatorController = 1;
+    public static double wheelDiameter = 4.0;
+    public static double wheelCircumfirence = wheelDiameter * Math.PI;
+
+    public static int elevator = 31;
+    public static int wrist = 32;
+    public static int vertroller = 33;
+    public static int hroller = 34;
+    public static int intakeSolenoid = 0;
+
+    public static double elevator_kP = 0.1;
+    public static double wrist_kP = 0.1;
+    public static double vertroller_kP = 0.1;
+
+    public static double bottomConePosition = 1.5;
+    public static double middleConePosition = 1.6;
+    public static double bottomCubePosition = 1.7;
+    public static double middleCubePosition = 1.8;
+    public static double topCubePosition = 1.9;
+
+    public static double pickupCone = 2.0;
+    public static double pickupCube = 2.1;
+    
+    public static double doubleSubPosition = 2.2;
+    public static double singleSubPosition = 2.3;
+
+
   }
 }
