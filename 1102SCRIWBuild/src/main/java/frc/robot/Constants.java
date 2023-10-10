5c3c6885dde72ad720cacfbde0569a414177b0c7
KPCar <146141777+KPCar@users.noreply.github.com>
2023-10-10 19:40:24 -0400

    Update Constants.java
    I added ID for my Motor

--- 1102SCRIWBuild/src/main/java/frc/robot/Constants.java
+++ 1102SCRIWBuild/src/main/java/frc/robot/Constants.java
@@ -15,5 +15,69 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+
+    public static final int steerFrontLeft = 10;
+    public static final int steerFrontRight = 11; 
+    public static final int steerBackLeft = 12;
+    public static final int steerBackRight = 13;
+    public static int steerMotorCurrentLimit = 45;
+
+    public static final int driveFrontLeft = 14;
+    public static final int driveFrontRight = 15;
+    public static final int driveBackRight = 16;
+    public static final int driveBackLeft = 17; 
+    public static int driveMotorCurrentLimit = 55;
+
+    public static final int frontLeftCANcoder = 18;
+    public static final int frontRightCANcoder = 19;
+    public static final int backLeftCANcoder = 20;
+    public static final int backRightCANcoder = 21;
+   
+     public static int elevator = 31;
+    public static int wrist = 32;
+    public static int vertroller= 33;
+    public static int hroller = 34;
+    public static int intakeSolenoid = 0;
+    public static int driverController = 0;
+    public static int operatorController = 1;
+    public static double wheelDiameter = 4.0;
+    public static double wheelCircumfrance = wheelDiameter * Math.PI;
+
+    public static double elevator_KP = 0.1;
+    public static double wrist_KP = 0.1;
+    public static double vertRollerKP = 0.1;
+
+    public static double bottomConePosition = 1.5;
+    public static double middleConePosition = 1.6; 
+    public static double bottomCubePosition = 1.7;
+    public static double middleCubePosition = 1.8;
+    public static double topCubePosition = 1.9; 
+    public static double pickupCone = 2.0;
+    public static double pickupCube = 2.1;
+
+
+
+
+    public class SuperSetpoint(){
+      public double elevatorSetpoint;
+      public double wristSetpoint;
+      public double SuperSetpoint(double)
+        theElevator= Elevator;
+    }
+
+
+
+    
+
+    
+
+
+
+    
+   
+
+
+
   }
 }
+
